262ee7f97b618156a48f64c57d76b11011b7906b
tylerfowle <tylerfowle@gmail.com>
2017-08-08 15:10:30 -0500

    hs window switcher

--- init.lua
+++ init.lua
@@ -166,6 +166,32 @@ end
 
 
 
+-- alt+tab window switcher
+hs.window.switcher.ui.textColor = {0.9,0.9,0.9}
+hs.window.switcher.ui.fontName = 'Hack'
+hs.window.switcher.ui.textSize = 14 -- in screen points
+hs.window.switcher.ui.highlightColor = {0.8,0.5,0,0.8}  -- highlight color for the selected window
+hs.window.switcher.ui.backgroundColor = {0.2,0.2,0.2,1}
+hs.window.switcher.ui.onlyActiveApplication = false -- only show windows of the active application
+hs.window.switcher.ui.showTitles = false  -- show window titles
+hs.window.switcher.ui.titleBackgroundColor = {0,0,0}
+hs.window.switcher.ui.showThumbnails = false  -- show window thumbnails
+hs.window.switcher.ui.thumbnailSize = 32  -- size of window thumbnails in screen points
+hs.window.switcher.ui.showSelectedThumbnail = true  -- show a larger thumbnail for the currently selected window
+hs.window.switcher.ui.selectedThumbnailSize = 600
+hs.window.switcher.ui.showSelectedTitle = false  -- show larger title for the currently selected window
+
+-- set up your windowfilter
+switcher = hs.window.switcher.new() -- default windowfilter: only visible windows, all Spaces
+switcher_space = hs.window.switcher.new(hs.window.filter.new():setCurrentSpace(true):setDefaultFilter{}) -- include minimized/hidden windows, current Space only
+switcher_browsers = hs.window.switcher.new{'Safari','Google Chrome'} -- specialized switcher for your dozens of browser windows :)
+
+-- alternatively, call .nextWindow() or .previousWindow() directly (same as hs.window.switcher.new():next())
+hs.hotkey.bind('alt','tab','Next window',hs.window.switcher.nextWindow)
+-- you can also bind to `repeatFn` for faster traversing
+hs.hotkey.bind('alt-shift','tab','Prev window',hs.window.switcher.previousWindow,nil,hs.window.switcher.previousWindow)
+
+
 
 -- aliases
 local mash 	 = {"cmd", "alt", "ctrl"}
@@ -200,5 +226,6 @@ end)
 -- local monitorWatcher = hs.screen.watcher.new(applyWindowLayout)
 local monitorWatcher = hs.screen.watcher.new(createCorrectSpaces)
 monitorWatcher:start()
+
 
 
