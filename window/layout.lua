24040c49de89ab7b7162b4e0fa916f6a38a29947
tylerfowle <tylerfowle@gmail.com>
2017-08-22 10:27:14 -0500

    setup mobile layout

--- window/layout.lua
+++ window/layout.lua
@@ -6,16 +6,14 @@ function applyWorkLayout()
   -- local thunder1 =    hs.screen.allScreens()[2]:name()
   -- local thunder2 =    hs.screen.allScreens()[3]:name()
 
+  local primaryScreen = hs.screen.primaryScreen() -- primary screen
+  local mainScreen = hs.screen.mainScreen() -- currently focused window screen
+
   local rightScreen = hs.screen.primaryScreen()
   local middleScreen = hs.screen.primaryScreen():toWest()
   local leftScreen = hs.screen.primaryScreen():toWest():toWest()
 
 
-
-
-  print(laptopScreen)
-  print(thunder1)
-  print(thunder2)
 
   print(rightScreen)
   print(middleScreen)
@@ -25,16 +23,13 @@ function applyWorkLayout()
 
 
   local workLayout = {
-    {"Google Chrome",  nil, leftScreen, hs.layout.right50,    nil, nil},
-    {"Photoshop CC", nil, leftScreen, hs.layout.left50, nil, nil},
-
-    {"iTerm2", nil, middleScreen, hs.layout.right70, nil, nil},
-
-    {"Mail", nil, rightScreen, hs.layout.left50, nil, nil},
-    {"Calendar", nil, rightScreen, hs.layout.right50, nil, nil},
-
-    {"Spotify", nil, rightScreen, hs.layout.maximized, nil, nil},
-    {"Slack", nil, rightScreen, hs.layout.maximized, nil, nil},
+    {"Google Chrome", nil, leftScreen,   hs.layout.right50,   nil, nil},
+    {"Photoshop CC",  nil, leftScreen,   hs.layout.left50,    nil, nil},
+    {"iTerm2",        nil, middleScreen, hs.layout.right70,   nil, nil},
+    {"Mail",          nil, rightScreen,  hs.layout.left50,    nil, nil},
+    {"Calendar",      nil, rightScreen,  hs.layout.right50,   nil, nil},
+    {"Spotify",       nil, rightScreen,  hs.layout.maximized, nil, nil},
+    {"Slack",         nil, rightScreen,  hs.layout.maximized, nil, nil},
   }
 
   hs.layout.apply(workLayout)
@@ -46,14 +41,22 @@ end
 ---------------------------------------------------------------------------
 function applyMobileLayout()
 
-  local laptopScreen = hs.screen.allScreens()[1]:name()
+  local primaryScreen = hs.screen.primaryScreen() -- primary screen
+  local mainScreen = hs.screen.mainScreen() -- currently focused window screen
+
+  local rightScreen = hs.screen.primaryScreen()
+  local middleScreen = hs.screen.primaryScreen():toWest()
+  local leftScreen = hs.screen.primaryScreen():toWest():toWest()
+
 
   local mobileLayout = {
-    {"Google Chrome",  nil, laptopScreen, hs.layout.left50,    nil, nil},
-    {"Mail", nil, laptopScreen, hs.layout.left50, nil, nil},
-
-    {"iTerm2", nil, laptopScreen, hs.layout.left50, nil, nil},
-    {"Spotify", nil, laptopScreen, hs.layout.left50, nil, nil},
+    {"Google Chrome", nil, primaryScreen, hs.layout.maximized, nil, nil},
+    {"Photoshop CC",  nil, primaryScreen, hs.layout.maximized, nil, nil},
+    {"iTerm2",        nil, primaryScreen, hs.layout.maximized, nil, nil},
+    {"Mail",          nil, primaryScreen, hs.layout.maximized, nil, nil},
+    {"Calendar",      nil, primaryScreen, hs.layout.maximized, nil, nil},
+    {"Spotify",       nil, primaryScreen, hs.layout.maximized, nil, nil},
+    {"Slack",         nil, primaryScreen, hs.layout.maximized, nil, nil},
   }
 
   hs.layout.apply(mobileLayout)
