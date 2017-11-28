3fb3968021f8e43a8ef0da7e87ac5809d75be23f
tylerfowle <tylerfowle@gmail.com>
2017-11-28 13:17:49 -0600

    add border to focused window

--- window/mission-control.lua
+++ window/mission-control.lua
@@ -12,3 +12,35 @@ function activateMissionControl()
 
 end
 
+
+
+
+
+
+-- DRAW BORDER AROUND ACTIVE WINDOW
+---------------------------------------------------------------------------
+global_border = nil
+
+function redrawBorder()
+    win = hs.window.focusedWindow()
+    if win ~= nil then
+        top_left = win:topLeft()
+        size = win:size()
+        if global_border ~= nil then
+            global_border:delete()
+        end
+        global_border = hs.drawing.rectangle(hs.geometry.rect(top_left['x'], top_left['y'], size['w'], size['h']))
+        global_border:setStrokeColor({["red"]=1,["blue"]=1,["green"]=0,["alpha"]=0.65})
+        global_border:setFill(false)
+        global_border:setStrokeWidth(2)
+        global_border:show()
+    end
+end
+
+redrawBorder()
+
+allwindows = hs.window.filter.new(nil)
+allwindows:subscribe(hs.window.filter.windowCreated, function () redrawBorder() end)
+allwindows:subscribe(hs.window.filter.windowFocused, function () redrawBorder() end)
+allwindows:subscribe(hs.window.filter.windowMoved, function () redrawBorder() end)
+allwindows:subscribe(hs.window.filter.windowUnfocused, function () redrawBorder() end)
