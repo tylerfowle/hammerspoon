05e98a8b23434a4471442d0ae948bc0800d5a02b
tylerfowle <tylerfowle@gmail.com>
2021-09-02 09:54:43 -0500

    add sleep and wake watchers

--- watchers/init.lua
+++ watchers/init.lua
@@ -5,3 +5,25 @@
 -- change window layout on monitor configuration change
 -- local monitorWatcher = hs.screen.watcher.new(applyWindowLayout)
 -- monitorWatcher:start()
+
+
+
+function sleepCallback( eventType )
+
+	local appDLM = hs.application.find("xScope")
+	if(appDLM ~= nil) then
+		appDLM:kill()
+	end
+
+	local appDLM = hs.application.find("DisplayLinkUserAgent")
+	if(appDLM ~= nil) then
+		appDLM:kill()
+	end
+end
+sleepWatcher = hs.caffeinate.watcher.new(sleepCallback):start()
+
+
+function wakeCallback( eventType )
+	hs.application.launchOrFocus("DisplayLink Manager")
+end
+wakeWatcher = hs.caffeinate.watcher.new(wakeCallback):start()
